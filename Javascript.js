0a9929c95307713d0ae87e5cb2064a3fd6b680b0
as & Anitha <student@epicodus.com>
2015-09-28 20:51:11 -0700

    js practice

---
+++ Javascript.js
@@ -0,0 +1,19 @@
+jQuery(document).ready(function(){
+$("h1").click(function() {
+  alert("This is header");
+
+});
+
+$("p").dblclick(function() {
+
+  alert("this is paragraph");
+  alert("This is text from lorem ipsum");
+
+});
+
+$ ("img").click(function() {
+  alert("This is picture of cat");
+
+
+});
+});
